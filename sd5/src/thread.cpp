015006c669c7a6d3e63a2bc263c6479c0bacc2f8
tocchan <christopher.forseth@gmail.com>
2017-06-08 12:54:57 -0500

    Thread Example Code from Class.

--- sd5/src/thread.cpp
+++ sd5/src/thread.cpp
@@ -61,11 +61,133 @@
 /* EXTERNAL FUNCTIONS                                                   */
 /*                                                                      */
 /************************************************************************/
+
+struct thread_pass_data_t
+{
+   thread_cb cb;
+   void *arg;
+};
+
+//------------------------------------------------------------------------
+static DWORD WINAPI ThreadEntryPointCommon( void *arg ) 
+{
+   thread_pass_data_t *pass_ptr = (thread_pass_data_t*)arg;
+
+   pass_ptr->cb( pass_ptr->arg );
+   delete pass_ptr;
+   return 0;
+}
+
+//------------------------------------------------------------------------
+// Creates a thread with the entry point of cb, passed data
+thread_handle_t ThreadCreate( thread_cb cb, void *data )
+{
+   // handle is like pointer, or reference to a thread
+   // thread_id is unique identifier
+   thread_pass_data_t *pass = new thread_pass_data_t();
+   pass->cb = cb;
+   pass->arg = data;
+
+   DWORD thread_id;
+   thread_handle_t th = ::CreateThread( nullptr,   // SECURITY OPTIONS
+      0,                         // STACK SIZE, 0 is default
+      ThreadEntryPointCommon,    // "main" for this thread
+      pass,                     // data to pass to it
+      0,                         // initial flags
+      &thread_id );              // thread_id
+
+   return th;
+}
+
+//------------------------------------------------------------------------
+void ThreadSleep( uint ms )
+{
+   ::Sleep( ms );
+}
+
+//------------------------------------------------------------------------
+void ThreadYield()
+{
+   ::SwitchToThread();
+}
+
+//------------------------------------------------------------------------
+// Releases my hold on this thread.
+void ThreadDetach( thread_handle_t th )
+{
+   ::CloseHandle( th );
+}
+
+//------------------------------------------------------------------------
+void ThreadJoin( thread_handle_t th ) 
+{
+   ::WaitForSingleObject( th, INFINITE );
+   ::CloseHandle( th );
+}
+
+
+
+//------------------------------------------------------------------------
+void GenerateGarbageWork( void *data ) 
+{
+   for (uint i = 0; i < 1000; ++i) {
+      printf( "Garbage Thread: %i\n", i );
+   }
+}
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+void GenerateGarbage( char const *filename, size_t byte_count ) 
+{
+   thread_handle_t th = ThreadCreate( GenerateGarbageWork, nullptr );
+   ThreadJoin( th );
+}
+
+#include <queue>
+ThreadSafeQueue<std::string> gMessages;
+bool gLoggerThreadRunning = true;
+
+void LoggerThread( void* )
+{
+   FILE *fh = fopen( "log.log", "w+" );
+   if (nullptr == fh) {
+      return; 
+   }
+
+   std::string msg;
+   while (gLoggerThreadRunning) {
+      if (gMessages.pop(&msg)) {
+         fprintf( fh, "%s\n", msg.c_str() );
+      }
+   }
+
+   fclose(fh);
+}
+
+void LogPrint( char const *msg ) 
+{
+   gMessages.push( msg );
+}
+
+//------------------------------------------------------------------------
 void ThreadDemo()
 {
-   int a = 10;
-   a += 5;
-   printf( "a = %i\n", a );
+   // GenerateGarbage( "garbage.dat", 50 * 1024 * 1024 );
+
+
+   thread_handle_t th = ThreadCreate( LoggerThread, nullptr );
+   
+   for (uint i = 0; i < 1000; ++i) {
+      LogPrint( "Hello" );
+      LogPrint( "Goodbye" );
+      LogPrint( "Do we need more stuff?" );
+   }
+
+   ThreadJoin( th );
+
+   for (uint i = 0; i < 10; ++i) {
+      printf( "Main Thread: %i\n", i );
+   }
 }
 
 /************************************************************************/
--- sd5/src/thread.h
+++ sd5/src/thread.h
@@ -7,11 +7,15 @@
 /************************************************************************/
 #include "common.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 /************************************************************************/
 /*                                                                      */
 /* DEFINES AND CONSTANTS                                                */
 /*                                                                      */
 /************************************************************************/
+// Infoknowledge Management System
 
 /************************************************************************/
 /*                                                                      */
@@ -24,6 +28,8 @@
 /* TYPES                                                                */
 /*                                                                      */
 /************************************************************************/
+typedef void* thread_handle_t;
+typedef void (*thread_cb)( void* );
 
 /************************************************************************/
 /*                                                                      */
@@ -37,6 +43,104 @@
 /*                                                                      */
 /************************************************************************/
 
+// Mutual Exclusive [mutex]: 
+// Fair:  Serviced in order of request.
+// Recursive: Someone is allowed to lock multiple times.
+class CriticalSection 
+{
+   public:
+      CriticalSection()
+      {
+         InitializeCriticalSection( &cs );
+      }
+
+      ~CriticalSection()
+      {
+         DeleteCriticalSection( &cs );
+      }
+
+      void lock()
+      {
+         EnterCriticalSection( &cs );
+      }
+
+      void unlock()
+      {
+         LeaveCriticalSection( &cs );
+      }
+
+   public: 
+      CRITICAL_SECTION cs;
+};
+
+class ScopeCriticalSection
+{
+   public:
+      ScopeCriticalSection( CriticalSection *ptr ) 
+      {
+         cs_ptr = ptr;
+         cs_ptr->lock();
+      }
+
+      ~ScopeCriticalSection()
+      {
+         cs_ptr->unlock();
+      }
+
+   public:
+      CriticalSection *cs_ptr;
+};
+
+#define COMBINE1(X,Y) X##Y
+#define COMBINE(X,Y) COMBINE1(X,Y)
+#define SCOPE_LOCK( csp ) ScopeCriticalSection COMBINE(__scs_,__LINE__)(csp)
+
+#include <queue>
+
+template <typename T>
+class ThreadSafeQueue
+{
+   public:
+      void push( T const &v ) 
+      {
+         SCOPE_LOCK(&m_lock);
+         m_queue.push( v );
+      }
+
+      bool empty() 
+      {
+         SCOPE_LOCK(&m_lock);
+         bool result = m_queue.empty();
+
+         return result;
+      }
+
+      bool pop( T *out )
+      {
+         SCOPE_LOCK(&m_lock);
+
+         if (m_queue.empty()) {
+            return false; 
+         } else {
+            *out = m_queue.front();
+            m_queue.pop();
+            return true;
+         }
+      }
+
+      T front() 
+      {
+         SCOPE_LOCK(&m_lock);
+         T result = m_queue.front(); 
+
+         return result;
+      }
+
+   public:
+      std::queue<T> m_queue;
+      CriticalSection m_lock;
+};
+
 /************************************************************************/
 /*                                                                      */
 /* GLOBAL VARIABLES                                                     */
@@ -48,6 +152,15 @@
 /* FUNCTION PROTOTYPES                                                  */
 /*                                                                      */
 /************************************************************************/
+
+// Creates a thread with the entry point of cb, passed data
+thread_handle_t ThreadCreate( thread_cb cb, void *data );
+
+void ThreadSleep( uint ms );
+
+// Releases my hold on this thread [one of these MUST be called per create]
+void ThreadDetach( thread_handle_t th );
+void ThreadJoin( thread_handle_t th );
 
 void ThreadDemo();
 
